4cc64b578f33feb94abb3fa87987139263e2ef20
perat1 <tuomo.pera@aalto.fi>
2017-02-20 19:03:16 +0200

    added IVT, updated memmap. Could not access to seq since .data and .bss was not intialized properly. Needed to add data values to access .bss

--- game.s
+++ game.s
@@ -1,4 +1,6 @@
 .globl _start
+    //b notmain
+    //b hang
 _start:
     //interupt vector table (IVT), these values are relative jumps.
     ldr pc,reset_handler
@@ -25,44 +27,44 @@ fiq_handler:         .word hang
 irq_routine:
     b .
 
-
 seq:
-    .byte    3
-    .byte    2
-    .byte    1
-    .byte    2
-    .byte    3
-    .byte    0
-    .byte    2
-    .byte    0
-    .byte    2
-    .byte    3
-    .byte    1
-    .byte    2
-    .byte    0
-    .byte    3
-    .byte    2
-    .byte    1
-    .text
+    .byte    25
+    .byte    24
+    .byte    23
+    .byte    24
+    .byte    25
+    .byte    22
+    .byte    24
+    .byte    22
+    .byte    24
+    .byte    25
+    .byte    23
+    .byte    24
+    .byte    22
+    .byte    25
+    .byte    24
+    .byte    23
     .align 2
 
 //could be bytes but shouldnt risk misalignment
+.data
 expected_idx:
     .word 0
-    
+
+.data //data is neede so .bss gets initialized properly. (cannot access seq properly)
 seq_count_idx:
     .word 0
     
 .align 4
+
 hw_init:
     //param:   void 
     //returns: void
     //GPIO 12 and 13 are PWM needed for buzzer and display
-    //set pins 11, 15, 16, 18 as button input pins
     //pins are input by default so we need to specify them as pull up
     ldr r0, =0x20200000 //gpio base register
     mov r1, #2             //pullup
-    strb r1, [r0, #0x94]
+    str r1, [r0, #0x94]
 
     //wait period of 150 cycles needed as per doc
     mov r1, #150
@@ -70,8 +72,9 @@ wait1:
     subs r1, #1
     bne wait1
     
+    //set pins 4, 17, 18, 27 as button input pins
     //give the input pins clock so we can read them.
-    ldr r1, =0x58800
+    ldr r1, =0x8060010
     str r1, [r0, #0x98]
     
     //wait period of 150 cycles needed as per doc
@@ -80,13 +83,12 @@ wait2:
     subs r1, #1
     bne wait2
     
-    //set pins 21, 22, 23, 24 as button LED pins (output)
-    ldr r1, =0x1248
+    //set pins 22, 23, 24, 25 as button LED pins (output)
+    ldr r1, =0x9240
     str r1, [r0, #0x8]
     
     bx lr
     
-
 led_on:
     //param:  gpio number of the output pin (r0)
     //return: void
@@ -114,30 +116,67 @@ led_toggle:
     ldr r3, [r1, #0x34]
     tst r2, r3            //mask with our shift to see if the pin is high or low
     //both functions take gpio num as r0, it's already there
-    beq led_on          //was low
+    //not sure if unsafe none of the opcodes should have sideeffects with flags
+    beq led_on          //was low    
     bne led_off         //Was high
     bx lr
     
+led_level:
+    //param:  gpio number of the output pin (r0)
+    //return: int (0 for low, != 0 for high)
+    ldr r1, =0x20200000 //gpio base register
+    ldr r2, [r1, #0x34]    //load current status of pin levels
+    mov r3, #1              
+    lsl r3, r0              //shift 1 to correct place
+    and r0, r2, r3          //and to see if it's up or down and return it
+    bx lr
     
 reset_routine:
-    mov sp,#0x8000 //stack init is required
-    mov r0, #0x08000000 //for purpose of my emulator, would be linked at 0x8000 on rpi
+    mov r0, #0x08000//000 //for purpose of my emulator, would be linked at 0x8000 on rpi
     mov r1, #0 //location for interrupt vectors
     ldmia r0!, {r2-r9} // load IVT to stack
     stmia r1!, {r2-r9} // store IVT from stack to 0 addr
     ldmia r0!, {r2-r9} // load absolute addresses to stack
     stmia r1!, {r2-r9} // store absolute addresses from stack to 0 addr
+    mov sp,#0x8000 //stack init is required
     bl notmain
-    b hang
-    
-notmain: //not main because assembler might put overhead
+   
+notmain: //not main because assembler might put overhead (dwelch)
     bl hw_init
-    mov r0, #3 //btn test
-game_loop:
-    bl btn_press
-    beq .  
-sub r0, #1    
-b game_loop
+
+new:    
+    ldr r2, =seq_count_idx
+    ldrb r3, [r2]
+    ldr r1, =seq
+    ldrb r4, [r1, r3]
+    mov r0, r4
+
+    bl led_on
+
+    mov r1, #0xFFFFFF
+    asd:
+        subs r1, #1
+        bne asd
+    
+    bl led_toggle // shut led 
+    
+    mov r1, #0xFFFFFF
+    asd2:
+        subs r1, #1
+        bne asd2
+     
+    ldr r1, =seq_count_idx //next led to load
+    ldr r0, [r1]
+    add r0, #1               //new index
+    and r0, #0xF            //we have 15 states so just anding will do fine
+    str r0, [r1]
+     
+    b new
+    
+    mov r0, #22
+    bl led_toggle
+    
+    
 hang: b hang
 
 
@@ -145,6 +184,7 @@ hang: b hang
 my emulator didnt support co processors yet, needed to put this to thumb since
 thumb supports direct byte transfers (wtf arm?)
 */
+/*
 .thumb
 .thumb_func
 btn_press:
@@ -169,3 +209,5 @@ fail:
     mov r0, #0
 done:
     bx lr
+    .align 4 //align needed for some hard values
+*/
